5fa5a799c454c8f7734fd9da12a66ae765c03ec9
nilesh <nileshgholap46@gmail.com>
2022-11-20 10:31:28 +0530

    Push the code in FA

---
+++ sampleproject/src/sampleproject/FA_project.java
@@ -0,0 +1,13 @@
+package sampleproject;
+
+public class FA_project {
+	
+	public static void main(String[] args) {
+		String s="Ni le sh";
+		
+		String s1=s.replaceAll("\s", "");
+		System.out.println(s1);
+		
+	}
+
+}
